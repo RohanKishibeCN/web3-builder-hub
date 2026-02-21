fc966829dbf6aac5a92d035e0140811abf32fadf
RohanKishibeCN <xenosaga1987@gmail.com>
2026-02-21 11:47:50 +0800

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,8 +1,11 @@
 import * as React from "react"
-import { cn } from "@/lib/utils"
+
+function cn(...classes: (string | undefined | null | false)[]) {
+  return classes.filter(Boolean).join(' ');
+}
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("rounded-lg border bg-zinc-900 text-white shadow-sm", className)} {...props} />
+  <div ref={ref} className={cn("rounded-lg border border-zinc-800 bg-zinc-900 text-white shadow-sm", className)} {...props} />
 ))
 Card.displayName = "Card"
 
@@ -11,7 +14,7 @@ const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(({ className, ...props }, ref) => (
+const CardTitle = React.forwardRef<HTMLHeadingElement, React.HTMLAttributes<HTMLHeadingElement>>(({ className, ...props }, ref) => (
   <h3 ref={ref} className={cn("text-2xl font-semibold leading-none tracking-tight", className)} {...props} />
 ))
 CardTitle.displayName = "CardTitle"
